f2762343e4e343716d832de2b112ccb7e8434deb
baoxiny666 <13161074839@163.com>
2020-12-10 19:28:06 +0800

    可以使用Mapper文件进行写sql了

--- src/main/java/com/itheima/springboot_scan_bxy/mapper/SysAdMapper.java
+++ src/main/java/com/itheima/springboot_scan_bxy/mapper/SysAdMapper.java
@@ -2,7 +2,6 @@ package com.itheima.springboot_scan_bxy.mapper;
 
 
 import com.itheima.springboot_scan_bxy.entity.Sys_Ad;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,11 +9,11 @@ import java.util.List;
 
 @Repository
 public interface SysAdMapper {
-    @Select("select id," +
+   /* @Select("select id," +
             "ad_group adGroup," +
             "ad_image adImage," +
             "DATE_FORMAT(create_time,'%Y-%m-%d %H:%i:%s') createTime," +
             "DATE_FORMAT(update_time,'%Y-%m-%d %H:%i:%s') updateTime" +
-            " from sys_ad")
+            " from sys_ad")*/
     List<Sys_Ad> select();
 }
