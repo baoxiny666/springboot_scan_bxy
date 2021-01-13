c94a46421014431c2b4a88aa445e8084868b1d8c
baoxiny666 <13161074839@163.com>
2021-01-13 11:25:50 +0800

    更新静态资源文件  POM  META-INF

---
+++ src/main/resources/static/precache-manifest.7175fac761fd3a8b2b93fa01762ded64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8464b295af442a8cdd20",
+    "url": "/css/app.726d87d3.css"
+  },
+  {
+    "revision": "2f8a4ac6b55dba7d2e6a",
+    "url": "/css/chunk-vendors.1a580696.css"
+  },
+  {
+    "revision": "67946a21aeff9aef99e375dd766eaa71",
+    "url": "/img/favicon.67946a21.png"
+  },
+  {
+    "revision": "f47b04fbfd84b505904820b852852dc9",
+    "url": "/img/login-bg.f47b04fb.jpg"
+  },
+  {
+    "revision": "d114153c8bbcf0ca8a7861be81acdac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8464b295af442a8cdd20",
+    "url": "/js/app.3a7ad215.js"
+  },
+  {
+    "revision": "2f8a4ac6b55dba7d2e6a",
+    "url": "/js/chunk-vendors.93b59240.js"
+  },
+  {
+    "revision": "71a2641a4c3d307d9a19313d81f7c06e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
